00cb7a97ed35546313a161384ca7a98c552dc946
Zach Ploskey <zach@ploskey.com>
2017-07-21 12:45:06 -0700

    Remove about, sponsors & faq nav links

--- bottom_urls.php
+++ bottom_urls.php
@@ -1,14 +1,8 @@
 <?php
 
 $bottom_urls = array(
-    'about',
-    'about-zh_cn',
     'credits',
     'credits-zh_cn',
-    'sponsors',
-    'sponsors-zh_cn',
     'contact',
     'contact-zh_cn',
-    'faq',
-    'faq-zh_cn',
 );
