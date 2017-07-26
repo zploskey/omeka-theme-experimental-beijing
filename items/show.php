9393e99cbb899823d2828e216c40f01c57183fb3
Zach Ploskey <zach@ploskey.com>
2017-07-26 13:25:19 -0700

    Disable toggle animation

--- items/show.php
+++ items/show.php
@@ -213,7 +213,7 @@ jQuery(document).ready(function() {
         var collapse_selector = jQuery(this).attr('href');
         var toggle_switch = jQuery(this);
 
-        jQuery(collapse_selector).toggle(function() {
+        jQuery(collapse_selector).toggle(0, function() {
             if (jQuery(this).css('display') == 'none') {
                 jQuery('#expand-symbol', toggle_switch).html('+');
             } else {
