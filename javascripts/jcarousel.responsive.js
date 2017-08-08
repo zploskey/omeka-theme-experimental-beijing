455bbf1bfd6cbc946a7b39f92af6294fae533fcd
Zach Ploskey <zach@ploskey.com>
2017-08-08 12:29:58 -0700

    Make images full width

--- javascripts/jcarousel.responsive.js
+++ javascripts/jcarousel.responsive.js
@@ -5,13 +5,6 @@
         jcarousel
             .on('jcarousel:reload jcarousel:create', function () {
                 var width = jcarousel.innerWidth();
-
-                if (width >= 600) {
-                    width = width / 3;
-                } else if (width >= 350) {
-                    width = width / 2;
-                }
-
                 jcarousel.jcarousel('items').css('width', width + 'px');
             })
             .jcarousel({
