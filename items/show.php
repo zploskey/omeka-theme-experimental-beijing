c25fa080dd8c4395dbf7a53f0a979965a96d64b9
Zach Ploskey <zach@ploskey.com>
2018-04-18 17:06:45 -0700

    Show people page about info next to headshot
    - Displays their name (Title) above everything.
    - Makes each metadata section have class element-set, removing the
      div with this class that previously wrapped them all.
    - Places About section to right of picture on wide layout
    - Hides the collapsiple header for About on Person items.
    - Some code cleanup for cleaner html output.
    
    Fixes zploskey/ExperimentalBeijing#27. Now quite close to spec.

--- items/show.php
+++ items/show.php
@@ -1,6 +1,13 @@
+<?php $isPerson = get_current_record('item')->getItemType()->name === "Person"; ?>
+
 <?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bodyclass' => 'items show')); ?>
 
 <div id="item-top">
+<?php if ($isPerson): ?>
+    <h1><?php
+        echo metadata('item', array('Dublin Core', 'Title'), array('no_escape' => true));
+    ?></h1>
+<?php endif; ?>
     <div id="item-top-media">
         <?php if ((get_theme_option('Item FileGallery') == 0) && metadata('item', 'has files')): ?>
         <?php echo files_for_item(
@@ -39,17 +46,20 @@
         ?>
     </div>
 
+<?php if (!$isPerson): ?>
     <div class="placard">
-        <h1><?php
-            echo metadata('item', array('Dublin Core', 'Title'), array('no_escape' => true));
-        ?></h1>
+        <h1>
+            <?php echo metadata('item', array('Dublin Core', 'Title'), array('no_escape' => true)); ?>
+        </h1>
     <?php
     $creators = array_unique(metadata('item', array('Dublin Core', 'Creator'), 'all'));
-    foreach($creators as $creator): ?>
+    foreach($creators as $creator):
+    ?>
         <h3><?php echo $creator; ?></h3>
     <?php endforeach; ?>
         <h3><?php echo metadata('item', array('Dublin Core', 'Date Created')); ?></h3>
     </div>
+<?php endif; ?>
 </div>
 
 <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
@@ -64,12 +74,14 @@ foreach ($elementSets as $elementName => $elementInfo) {
 
 $aboutElements = array(
     'Title',
+    // Person About metadata
     'Last Name',
     'First Name',
     'Gender',
     'Culture',
     'Birth Date',
     'Death Date',
+    // Other item metadata
     'Is Part Of',
     'Creator',
     'Contributor',
@@ -89,47 +101,50 @@ $roles = array_keys($roleMap);
 
 ?>
 
-<div class="element-set">
-
-<div id="item-about">
+<div id="item-about" class="element-set<?php echo $isPerson ? ' person' : ''; ?>">
     <a href="#about-section" class="section-toggle">
         <h2><?php echo __('About'); ?><span id="expand-symbol">-</span></h2>
     </a>
     <div id="about-section">
         <div class="element-col-name"></div>
         <div class="element-col-text"></div>
-<?php foreach ($aboutElements as $elementName): ?>
-    <?php $canHaveRoles = isset($roleMap[$elementName]); ?>
-    <?php if (isset($elements[$elementName])): ?>
-        <?php $texts = $elements[$elementName]; ?>
+<?php
+    foreach ($aboutElements as $elementName):
+        // Don't display title here on People items
+        if ($isPerson AND $elementName === 'Title') {
+            continue;
+        }
+        $canHaveRoles = isset($roleMap[$elementName]);
+        if (isset($elements[$elementName])):
+            $texts = $elements[$elementName];
+?>
         <div id="<?php echo text_to_id(html_escape("$elementName")); ?>" class="element">
-            <?php
+<?php
             if ($canHaveRoles && isset($elements[$roleMap[$elementName]])) {
                 $roles = $elements[$roleMap[$elementName]];
             } else {
                 $roles = array();
             }
-            ?>
-            <div class="element-cell element-cell-name">
-                <?php
+?>
+            <div class="element-cell element-cell-name"><?php
                 echo html_escape(__($elementName));
                 echo $roles ? ' (' . __('role') . '):' : ':';
-                ?>
-            </div>
+            ?></div>
             <div class="element-cell">
-            <?php foreach ($texts as $i => $text): ?>
+<?php       foreach ($texts as $i => $text): ?>
                 <div class="element-text">
                     <?php
                     if ($elementName == 'Birth Date' OR $elementName == 'Death Date') {
                         $text = preg_replace('/(.*)-0-0$/', '\1', $text, 1);
                     }
+                    echo $text;
+                    if (isset($roles[$i])) {
+                        echo ' (' . $roles[$i] . ')';
+                    }
                     ?>
-                    <?php echo $text; ?>
-                    <?php if (isset($roles[$i])): ?>
-                        (<?php echo $roles[$i]; ?>)
-                    <?php endif; ?>
+
                 </div>
-            <?php endforeach; ?>
+<?php       endforeach; ?>
             </div>
         </div>
     <?php endif; ?>
@@ -161,20 +176,18 @@ $roles = array_keys($roleMap);
             </div>
         </div>
     <?php endif; ?>
-        <!-- end element -->
-    </div><!-- end about-section -->
-</div><!-- end item-about -->
+    </div>
+</div>
 
 <?php if (metadata('item', 'has tags')): ?>
 
-<div id="item-keywords">
+<div id="item-keywords" class="element-set">
     <a href="#keywords-section" class="section-toggle">
         <h2>
             <?php echo __('Keywords'); ?>
             <span id="expand-symbol">+</span>
         </h2>
     </a>
-    <!-- The following prints a list of all tags associated with the item -->
     <div id="keywords-section">
         <div id="item-tags" class="element">
             <div class="element-text">
@@ -194,7 +207,7 @@ $roles = array_keys($roleMap);
 
 <?php if (isset($this->works)): ?>
 
-<div id="item-works">
+<div id="item-works" class="element-set">
 <a href="#works-section" class="section-toggle">
     <h2>
         <?php echo __('Works'); ?>
@@ -222,10 +235,10 @@ $roles = array_keys($roleMap);
                 <?php echo $date_created; ?>
             </div>
             <?php endif; ?>
-        </div><!-- end class="item-meta" -->
-    </div><!-- end class="items hentry" -->
+        </div>
+    </div>
 <?php endforeach; ?>
-</div><!-- end id="works-section" class="items-list" -->
+</div>
 
 <?php endif; ?>
 
@@ -246,7 +259,7 @@ $moreInfoElements = array(
 );
 ?>
 
-<div id="item-more-info">
+<div id="item-more-info" class="element-set">
     <a href="#more-info-section" class="section-toggle">
         <h2>
             <?php echo __('More Info'); ?>
@@ -275,7 +288,6 @@ $moreInfoElements = array(
     <?php endif; ?>
 <?php endforeach; ?>
 
-        <!-- The following prints a citation for this item. -->
         <div id="item-citation" class="element">
             <div class="element-cell element-cell-name">
                 <?php echo __('Citation') . ':'; ?>
@@ -287,8 +299,6 @@ $moreInfoElements = array(
             </div>
         </div>
     </div>
-</div><!-- end more-info-section -->
-
-</div><!-- end element-set -->
+</div>
 
 <?php echo foot(); ?>
